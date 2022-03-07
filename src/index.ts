fa54d397ff5260c992479a5646344e331b42a26e
wa_su <waqas@itinsync.com>
2022-03-07 11:28:57 +0500

    NodeJS related changes and installed more packages

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import  express  from 'express'
 
-const app = express() 
+const app = express()
 
-app.get('/', (req) => {
-    
+const add = (a: number, b: number) => {
+    return a + b;
+}
+
+app.get('/', (req: any) => {
+   console.log(add(10,24))
+   return add(10,23)
 })
 
 app.listen(3001, () => {
     console.log("Started");
+    console.log(add(10,23))
 })
+console.log("Hello from NodeJS")
