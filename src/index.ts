a476f24660d04795527dc4d0bd44da5b22106b81
wa_su <waqas@itinsync.com>
2022-03-07 14:02:40 +0500

    making Index runable

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,46 @@
-import  express  from 'express'
+import express from 'express'
 
 const app = express()
+const { Kafka } = require('kafkajs')
 
-const add = (a: number, b: number) => {
-    return a + b;
-}
+app.get('/message', async (req: any, resp: any) => {
+    const kafka = new Kafka({
+        clientId: 'my-app',
+        brokers: ['localhost:9092'],
+    })
+    const producer = kafka.producer()
+
+    await producer.connect()
+    await producer.send({
+        topic: 'test-topic',
+        messages: [
+            { value: 'Hello Abd' },
+        ],
+    })
+
+    await producer.disconnect()
+});
 
 app.get('/sufyan', (req: any, resp: any) => {
-    resp.send('hello');
-})
+    resp.send('sufyan');
+});
+app.get('/messages',async (req: any, resp: any) => {
+    const kafka = new Kafka({
+        clientId: 'my-app',
+        brokers: ['localhost:9092'],
+    })
+    const consumer = kafka.consumer({ groupId: 'test-group' })
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+    await consumer.run({
+        eachMessage: async ({ message }) => {
+            console.log({
+                value: message.value.toString(),
+            })
+        },
+    })
+});
 
 app.listen(3001, () => {
     console.log("Started");
-    console.log(add(10,23))
 })
-console.log("Hello from NodeJS")
